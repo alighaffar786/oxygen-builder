aeaa67a64e660d10d5e9dfc1dd503a69105aefe0
alighaffar786 <alig.dtech@gmail.com>
2023-06-13 12:44:44 +0500

    oxygen

---
+++ wp-content/plugins/courses/index.php
@@ -0,0 +1,10 @@
+<?
+/*
+Plugin Name: courses
+Plugin URI: https://not.yet/
+Description: adding few products in wo-commerce
+Version: 0.1
+Author: ali
+Author URI: https://office.com/
+Text Domain: ali
+*/
